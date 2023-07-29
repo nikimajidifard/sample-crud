c4daced3a3cdb1f471df1a84f5795d3318a70a6b
nikimajidifard <nikimajidifard.mfa@gmail.com>
2023-07-29 13:31:12 +0330

    add api and controller service to container

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,7 +1,23 @@
+using Demo.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Dbcon"); // to get dbcon str
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllers(); //added
+builder.Services.AddEndpointsApiExplorer(); //added
 
 // Configure Swagger
 builder.Services.AddSwaggerGen();
