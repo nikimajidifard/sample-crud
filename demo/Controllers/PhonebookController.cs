27a6cff30f669fc5a025ca9fa8dfa533eddbbf72
nikimajidifard <nikimajidifard.mfa@gmail.com>
2023-07-29 14:32:49 +0330

    add get-del-add methods

---
+++ demo/Controllers/PhonebookController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Demo.Models;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Reflection.Metadata.Ecma335;
+
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppdbController : ControllerBase
+    {
+        private AppDbContext appdbcontext;
+        
+        public AppdbController(AppDbContext appdbcontext)
+        {
+            this.appdbcontext = appdbcontext;
+        }
+
+        [HttpGet]
+        [Route("GetPhones")]
+
+        public List<phonebook> GetPhones()
+        {
+            return appdbcontext.PhonebookEntries.ToList(); //return a list of all phonebooks data by get method 
+        }
+
+        [HttpGet]
+        [Route("GetSpecifiedPhone")]
+
+        public phonebook GetPhone(int id)
+        {
+            return appdbcontext.PhonebookEntries.Where(x => x.Id == id).FirstOrDefault(); //return the specified phonebook by id and get method 
+        }
+
+        [HttpPost]
+        [Route("AddPhone")]
+
+        void AddPhone(phonebook phone) // to add a phone by post method
+        {
+            appdbcontext.PhonebookEntries.Add(phone);
+            appdbcontext.SaveChanges();
+           // Console.WriteLine("phone added");
+        }
+
+
+        [HttpDelete]
+        [Route("DeletePhone")]
+        void DeletePhone(phonebook phone)
+        {
+            appdbcontext.PhonebookEntries.Remove(phone);
+            appdbcontext.SaveChanges();
+
+        }
+
+
+
+    }
+    
+}
